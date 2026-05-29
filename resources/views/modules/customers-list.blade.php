26272c088fb83da81760c101381590de0adacb76
sandas <sandazzzg1999@gmail.com>
2026-05-29 16:57:27 +0530

    done

--- resources/views/modules/customers-list.blade.php
+++ resources/views/modules/customers-list.blade.php
@@ -66,10 +66,10 @@
                                             <a href="{{ route('customers.edit', $customer) }}" class="text-blue-600 hover:text-blue-800 text-sm font-semibold">
                                                 <i class="fas fa-edit mr-1"></i>Edit
                                             </a>
-                                            <form action="{{ route('customers.destroy', $customer) }}" method="POST" style="display:inline;" onsubmit="return confirm('Are you sure?')">
+                                            <form action="{{ route('customers.destroy', $customer) }}" method="POST" style="display:inline;">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="text-red-600 hover:text-red-800 text-sm font-semibold">
+                                                <button type="button" onclick="showDeleteConfirm(this.closest('form'))" class="text-red-600 hover:text-red-800 text-sm font-semibold">
                                                     <i class="fas fa-trash mr-1"></i>Delete
                                                 </button>
                                             </form>
